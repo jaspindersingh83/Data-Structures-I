21a824f148ca3476e991942b30906ebfdbde4ec0
JASPINDER SING <jaspindersingh83@gmail.com>
2017-11-13 17:08:42 -0800

    Queues completed

--- src/queue.js
+++ src/queue.js
@@ -5,7 +5,45 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
-
+  constructor(options) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  enqueue(value) {
+    const newnode = {
+      value, // same as value :value
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = newnode;
+      this.length = 1;
+    } else {
+      this.tail.next = newnode;
+      this.tail = newnode;
+      this.length = this.length + 1;
+    }
+  }
+  dequeue() {
+    if (!this.head) {
+      return null;
+    }
+    const top = this.head.value;
+    if (this.head === this.tail) {
+      this.tail = null;
+      this.head = null;
+      this.length = 0;
+    } else {
+      const node = this.head.next;
+      this.head = node;
+      this.length = this.length - 1;
+    }
+    return top;
+  }
+  get size() {
+    return this.length;
+  }
 }
 
 module.exports = Queue;
